43be4fde8a36995ae3a3f1d13badb254c28434aa
Tanvee Dighe <tanvidighe1356@gmail.com>
2026-03-17 20:20:54 +0530

    Add files via upload

---
+++ Creating_Class.py
@@ -0,0 +1,45 @@
+#Class: Blueprint/Template ussed to create objects.
+#Class groups together Data(Variables/Attributes) and Funtions(Methods) in one place.
+#eg- Car Class can have attributes like colour, brand and methods like start(),stop().
+
+Class: DataTypes
+Objects: Variables
+
+#Class Container: used to Store Properties & funtionalities of realtime entity.
+
+#Create Class:
+class class_name:
+    #attributes (Variables)
+    #methods (Funtions)
+
+#Class without attributes or methods  
+class mango:
+    pass
+
+#Difference between mmethod & normal funtion:
+Funtion: independent block of code to perform specific task.
+method:  Funtion defined inside.
+
+#Rules of Class:
+1)Defining Class: Class is created using class keyword.
+-class_name should follow pascal rules
+
+2)Class_Name_Rules: must start with 'letter' or '_'
+-Can't start with number.
+-should not Python Keyword.
+eg. class, def, for
+-Use capatalized words (pascal)
+eg. CarModel, EmployeeDetails.
+
+3)Attributes and Methods:
+inside class----
+we can define: #variables--attributes or data members.
+               #funtions---called methods.
+Attributes & Methods belong to Objects created from Class.
+
+4)Indentation Rule: All class members(Var,Methods) must follow proper identation under class block.
+
+
+
+
+
